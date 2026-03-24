87e78df0b9df0413cd21301f2b6490005590ee5e
EM2024007453 <diego.barrio@nter.es>
2026-03-24 15:29:01 +0100

    Add controller

---
+++ NetDocker/Controllers/BankAccountController.cs
@@ -0,0 +1,40 @@
+﻿using Application.DTO;
+using Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class BankAccountController : Controller
+{
+    private readonly IBankAccountService _bankAccountService;
+
+    public BankAccountController(IBankAccountService bankAccountService)
+    {
+        _bankAccountService = bankAccountService;
+    }
+
+    [HttpGet("{id:guid}")]
+    public async Task<ActionResult<BankAccountDto>> Get(Guid id)
+    {
+        var account = await _bankAccountService.GetByIdAsync(id);
+        if (account == null) return NotFound();
+        return Ok(account);
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<BankAccountDto>>> GetAll()
+    {
+        var accounts = await _bankAccountService.GetAllAsync();
+        return Ok(accounts);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Guid>> Add([FromBody] BankAccountAddDto dto)
+    {
+        var id = await _bankAccountService.AddAsync(dto);
+        if (id == null) return BadRequest();
+        return CreatedAtAction(nameof(Get), new { id }, id);
+    }
+}
